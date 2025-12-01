4311aaff94e6bc45dd234a534af836f157559bae
Laila Seif <lailaseif710@gmail.com>
2025-12-01 01:58:05 -0500

    extra animations
    i used basic keyframes that we learn in term 1 and 2 (around that time) for some fun effects like hovering over each book and a very basic simple hero (colour changing bkg and slide in) its basic but doesnt look bad
    now i need to design the book detail page

--- details.php
+++ details.php
@@ -18,6 +18,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/keyframe.css" type="text/css">
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <title>Classic Centre - <?php echo $row['title']; ?> Details</title>
 </head>
@@ -37,6 +38,7 @@
     </header>
 
     <main id="content" class="main">
+
         <section>
             <?php
                 echo
@@ -57,8 +59,9 @@
         </section>
     </main>
 
-    <footer class="foot">
-        
+    <footer class="grid-con">
+        <p class="col-span-3">All rights reserved. 2025.</p>
+        <a href="#top" class="btt-button col-start-4 m-col-start-12 l-col-start-12"><img src="imgs/SVG/arrow.svg">GO TO TOP</a>
     </footer>
     <script src="js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
--- index.php
+++ index.php
@@ -15,11 +15,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/keyframe.css" type="text/css">
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <title>Classic Centre - Home</title>
 </head>
 <body class="site">
-    <h1 class="hidden">Bookstore</h1>
 
     <header class="grid-con main-header">
         <img src="imgs/SVG/logo.svg" class="col-span-3 m-col-span-2 m-col-start-1 l-col-span-2 l-col-start-1" alt="classic centre logo">
@@ -34,8 +34,16 @@
     </header>
 
     <main id="content" class="main">
+
+    <section class="hero grid-con">
+            <div class="hero-content full">
+                <h1>Keeping Classic Literature Accessible</h1>
+                <p>Experience the power of technology in preserving all kinds of old novels, classics, and historic pieces</p>
+            </div>
+        </section>
+    
         <section class="book-gallery">
-            <h2>The Collection</h2>
+            <h2 class="hidden">The Collection</h2>
             <div class="book-strip grid-con">
             <?php
                 while ($row = mysqli_fetch_array($results)) {
@@ -53,8 +61,9 @@
 </section>
     </main>
 
-    <footer class="foot">
-        
+    <footer class="grid-con">
+        <p class="col-span-3">All rights reserved. 2025.</p>
+        <a href="#top" class="btt-button col-start-4 m-col-start-12 l-col-start-12"><img src="imgs/SVG/arrow.svg">GO TO TOP</a>
     </footer>
     <script src="js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
